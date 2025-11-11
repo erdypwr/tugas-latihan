5b738b1541fca4fb98c1e0e6e9a0000ba49bc49d
erdypwr <100707582+erdypwr@users.noreply.github.com>
2025-11-11 09:19:30 +0800

    Delete 07 Assignment & operator aritmatika directory

--- 07 Assignment & operator aritmatika/SOAL 1 - Tabungan Bank/soal 1 tabungan bank.php
+++
@@ -1,7 +0,0 @@
-<?php  
-$saldoAwal = 1000000;  
-$bunga = 0.0025;  
-$bulan = 11;  
-$saldoAkhir = $saldoAwal * (1 + ($bunga * $bulan));  
-echo "Saldo akhir setelah ".$bulan." bulan adalah : Rp. ".$saldoAkhir;  
-?>
